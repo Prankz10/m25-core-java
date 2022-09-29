2750191a1972ee9ba08c856d8408a969d9919bc1
HP <HP@DESKTOP-8LDVUST>
2022-09-29 15:22:19 +0530

    updated

---
+++ Day3_ClassObjectAndConstructor/src/org/tnsindia/parameterized/ParameterizedConstructorDemo.java
@@ -0,0 +1,23 @@
+package org.tnsindia.parameterized;
+import java.util.Scanner;
+class Bike
+{
+	public int speed;
+	Bike(int s)
+	{
+		speed=s;
+		System.out.println("The speed is: "+speed+"km/hr");
+	}
+}
+public class ParameterizedConstructorDemo {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter the value of speed: ");
+		int speed=s.nextInt();
+		
+		Bike b=new Bike(speed);
+		s.close();
+	}
+
+}
