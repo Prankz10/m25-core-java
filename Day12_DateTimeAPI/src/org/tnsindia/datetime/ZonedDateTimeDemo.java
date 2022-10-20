af21b550d5d2a39fe1afd22604f8e80cfba29ea5
PRANIT <PRANIT@LAPTOP-U4LOFGGC>
2022-10-20 13:23:55 +0530

    First commit

---
+++ Day12_DateTimeAPI/src/org/tnsindia/datetime/ZonedDateTimeDemo.java
@@ -0,0 +1,14 @@
+package org.tnsindia.datetime;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+@SuppressWarnings("unused")
+public class ZonedDateTimeDemo {
+
+	public static void main(String[] args) {
+		ZonedDateTime obj1=ZonedDateTime.now();
+		System.out.println(obj1);
+	}
+
+}
