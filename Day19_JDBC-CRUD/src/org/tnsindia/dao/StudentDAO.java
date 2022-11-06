32f184ccb5ed61692997ed90d2dbcd7a4462ae63
Prankz10 <114399577+Prankz10@users.noreply.github.com>
2022-11-06 12:57:22 +0530

    first commit

---
+++ Day19_JDBC-CRUD/src/org/tnsindia/dao/StudentDAO.java
@@ -0,0 +1,30 @@
+package org.tnsindia.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class StudentDAO {
+	
+	public Connection createConnection()
+	{
+		String url="jdbc:mysql://127.0.0.1:3306/M25";
+		String user="root";
+		String password="sqlpassword";
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}
+		catch(ClassNotFoundException e)
+		{
+			System.out.println("Exception occurred "+e);
+		}
+		Connection c;
+		try {
+			c=DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			c=null;
+			System.out.println("Invalid crendentials");
+		}
+		return c;
+	}
+}
